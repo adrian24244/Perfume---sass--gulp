5f9614a7d3dca132d87f74124850a6d6d9958a08
ADRIAN <adrianmunoz080@gmail.com>
2023-09-19 21:19:33 -0500

    practica de html, sass y gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+// DEPENDENCIAS GULP
+const { src, dest, watch } = require("gulp");
+
+// DEPENDENCIAS CSS
+const sass = require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+
+function css(done) {
+
+    src("src/scss/**/*.scss")
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest("build/css"));
+
+    done();
+}
+
+function dev(done) {
+    watch("src/scss/**/*.scss", css);
+
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
